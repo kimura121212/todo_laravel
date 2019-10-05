9d5074ab162e3d1d7fb9c371d333595e4ee9479c
kimura121212 <ykimuranoi@gmail.com>
2019-10-06 03:15:55 +0900

    add scaffold

--- config/app.php
+++ config/app.php
@@ -155,6 +155,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        "Laralib\L5scaffold\GeneratorsServiceProvider",
+
 
     ],
 
